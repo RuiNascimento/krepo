b82e9611b5c4cf9660d470749a94a16c78eb61d8
RuiNascimento <rui_nascimento93@hotmail.com>
2019-01-02 17:36:12 +0000

    Create version.php
    trying some stuff

---
+++ version.php
@@ -0,0 +1,11 @@
+{
+    "config_version": "20181202_2151",
+    "config_url": "https://archive.org/download/KodiBuild-Rui/Kodi_build_64bit_windows.zip",
+    "config_md5": "2289453a7ef1a82dd1fbfe1ae3d2d184",
+    "test_version": "20181202_2151",
+    "test_url": "http://yourdomain.com/kodi/kodi.20181202_2151.zip",
+    "test_md5": "ec2d3ab7fe237122c37882258c7d4a7f",
+    "kodi_version": "17.6",
+    "kodi_url": "http://mirrors.kodi.tv/releases/android/arm/kodi-17.6-Krypton-armeabi-v7a.apk",
+    "kodi_md5": ""
+}
