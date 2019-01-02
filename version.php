c949a82b3327c0bc26a257b6f2dd9bd891173212
RuiNascimento <rui_nascimento93@hotmail.com>
2019-01-02 18:52:20 +0000

    Update version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 {
-    "config_version": "20181202_2151",
-    "config_url": "https://archive.org/download/KodiBuild-Rui/Kodi_build_64bit_windows.zip",
-    "config_md5": "2289453a7ef1a82dd1fbfe1ae3d2d184",
+    "config_version": "20190102_1846",
+    "config_url": "https://archive.org/download/KodiBuild-Rui/Kodi.20190102_1846.zip",
+    "config_md5": "dbc1ce07df3397299d99c5e9684a14c3",
     "test_version": "20181202_2151",
     "test_url": "http://yourdomain.com/kodi/kodi.20181202_2151.zip",
     "test_md5": "ec2d3ab7fe237122c37882258c7d4a7f",
