178fdcee17ae6f23deddc42bfc6c1c63d388a77c
RuiNascimento <rui_nascimento93@hotmail.com>
2019-01-03 12:49:39 +0000

    new config
    new config
    minor bug fixes

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 {
-    "config_version": "20190102_1846",
-    "config_url": "https://archive.org/download/KodiBuild-Rui/kodi.20190103_1036.zip",
-    "config_md5": "6f735ee7bb6741c0842ffc923e3b2561"
+    "config_version": "20190103_1239",
+    "config_url": "https://archive.org/download/KodiBuild-Rui/kodi.20190103_1239.zip",
+    "config_md5": "bcd516693c7875bdc97302d393082ddb"
 }
