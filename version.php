959ef5a5471d37d4e4bad4f8ad8ee0287e532ef0
RuiNascimento <rui_nascimento93@hotmail.com>
2019-01-03 10:42:14 +0000

    Update version.php

--- version.php
+++ version.php
@@ -1,11 +1,5 @@
 {
     "config_version": "20190102_1846",
-    "config_url": "https://archive.org/download/KodiBuild-Rui/Kodi.20190102_1846.zip",
-    "config_md5": "dbc1ce07df3397299d99c5e9684a14c3",
-    "test_version": "20181202_2151",
-    "test_url": "http://yourdomain.com/kodi/kodi.20181202_2151.zip",
-    "test_md5": "ec2d3ab7fe237122c37882258c7d4a7f",
-    "kodi_version": "17.6",
-    "kodi_url": "http://mirrors.kodi.tv/releases/android/arm/kodi-17.6-Krypton-armeabi-v7a.apk",
-    "kodi_md5": ""
+    "config_url": "https://archive.org/download/KodiBuild-Rui/kodi.20190103_1036.zip",
+    "config_md5": "6f735ee7bb6741c0842ffc923e3b2561"
 }
